db55852bf93cc54b508e30a201d9cdd5ed782229
odedraban <odedraban@post.runi.ac.il>
2024-11-15 22:29:06 +0200

    exercise 3

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,7 @@ public class Cheers {
             String anLetters = "AEFHILMNORSX";
             for (int i = 0; i < word.length(); i++){
                 if (anLetters.indexOf(UpperCaseWord.charAt(i)) == -1){
-                        System.out.println("Give me a " + UpperCaseWord.charAt(i) + ": " + UpperCaseWord.charAt(i) + "!");
+                        System.out.println("Give me a  " + UpperCaseWord.charAt(i) + ": " + UpperCaseWord.charAt(i) + "!");
                 }
                 else {
                         System.out.println("Give me an " + UpperCaseWord.charAt(i) + ": " + UpperCaseWord.charAt(i) + "!");
